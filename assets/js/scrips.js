b07e7a75ecf8a088af217cb6f8866253386d976f
juanmunoz1986 <juan_munoz91202@elpoli.edu.co>
2023-06-12 04:07:32 -0500

    pag_login_jq

---
+++ assets/js/scrips.js
@@ -0,0 +1,12 @@
+
+
+$("#login").click(function () { 
+    alert("login realizado con éxito");
+    window.location.href = "./login.html";
+});
+
+$("#Suscribir").click(function () { 
+    alert("Suscripción realizada con éxito");
+    
+    
+});
